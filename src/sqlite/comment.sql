541388d9e3c3d01ec6b7464d45d66218dfc6b3e7
Paul Ellenbogen <pe5@cs.princeton.edu>
2021-08-14 13:07:53 -0700

    Add missing sql files.

--- src/sqlite/comment.sql
+++ src/sqlite/comment.sql
@@ -9,26 +9,3 @@ CREATE TABLE IF NOT EXISTS comment (id INTEGER PRIMARY KEY,
                                     retrieved_on INTEGER,
                                     parent_id TEXT NOT NULL,
                                     parent_is_post BOOLEAN NOT NULL);
-
-
-CREATE VIRTUAL TABLE IF NOT EXISTS comment_fts USING fts5(author, subreddit, body, content = 'comment', content_rowid = 'id');
-
-CREATE TRIGGER IF NOT EXISTS comment_ai AFTER INSERT ON comment
-    BEGIN
-        INSERT INTO comment_fts (rowid, author, subreddit, body)
-        VALUES (new.id, new.author, new.subreddit, new.body);
-    END;
-
-CREATE TRIGGER IF NOT EXISTS comment_ad AFTER DELETE ON comment
-    BEGIN
-        INSERT INTO comment_fts (comment_fts, rowid, author, subreddit, body)
-        VALUES ('delete', old.id, old.author, old.subreddit, old.body);
-    END;
-
-CREATE TRIGGER  IF NOT EXISTS comment_au AFTER UPDATE ON comment
-    BEGIN
-        INSERT INTO comment_fts (comment_fts, rowid, author, subreddit, body)
-        VALUES ('delete', old.id, old.author, old.subreddit, old.body);
-        INSERT INTO comment_fts (rowid, author, subreddit, body)
-        VALUES (new.id, new.author, new.subreddit, new.subreddit);
-    END;
---
+++ src/sqlite/comment_fts.sql
@@ -0,0 +1,21 @@
+CREATE VIRTUAL TABLE IF NOT EXISTS comment_fts USING fts5(author, subreddit, body, content = 'comment', content_rowid = 'id');
+
+CREATE TRIGGER IF NOT EXISTS comment_ai AFTER INSERT ON comment
+    BEGIN
+        INSERT INTO comment_fts (rowid, author, subreddit, body)
+        VALUES (new.id, new.author, new.subreddit, new.body);
+    END;
+
+CREATE TRIGGER IF NOT EXISTS comment_ad AFTER DELETE ON comment
+    BEGIN
+        INSERT INTO comment_fts (comment_fts, rowid, author, subreddit, body)
+        VALUES ('delete', old.id, old.author, old.subreddit, old.body);
+    END;
+
+CREATE TRIGGER  IF NOT EXISTS comment_au AFTER UPDATE ON comment
+    BEGIN
+        INSERT INTO comment_fts (comment_fts, rowid, author, subreddit, body)
+        VALUES ('delete', old.id, old.author, old.subreddit, old.body);
+        INSERT INTO comment_fts (rowid, author, subreddit, body)
+        VALUES (new.id, new.author, new.subreddit, new.subreddit);
+    END;
